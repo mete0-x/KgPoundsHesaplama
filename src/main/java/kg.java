9b550dfc0924c995bcb06b4e50eaabbadd367864
mete0-x <meteturgutttt@gmail.com>
2023-01-31 21:01:07 +0300

    KgPoundsHesaplama
    KgPoundsHesaplama

---
+++ src/main/java/kg.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class kg {
+    public static void main(String[] args){
+    double pounds = 2.2;
+
+        System.out.println("Kilogram        Pounds");
+
+for(int i= 1; i <= 199; i+=2){
+    System.out.println(i+ "               "+(i*pounds));
+ }
+
+   }
+}
